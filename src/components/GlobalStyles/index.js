25586fa92db6852da92fc0a7c24a75329c6d09a2
Howard <howard.nguyen1@vmogroup.com>
2024-07-12 11:47:24 +0700

    cai đặt route

---
+++ src/components/GlobalStyles/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './GlobalStyles.scss'
+
+const GlobalStyles = ({children}) => {
+  return (
+    <div>{children}</div>
+  )
+}
+
+export default GlobalStyles;
---
+++ src/components/Layout/HeaderOnly/index.js
@@ -0,0 +1,15 @@
+import Header from "~/components/Layout/components/Header";
+import React from 'react'
+
+const HeaderOnly = ({ children}) => {
+  return (
+    <div>
+        <Header/>
+        <div className="container">
+          {children}
+        </div>
+    </div>
+  )
+}
+
+export default HeaderOnly
