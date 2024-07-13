03f0889cfc47f9507ee12cbb3d9626c8acfb8142
Howard <howard.nguyen1@vmogroup.com>
2024-07-13 10:12:44 +0700

    Dựng khung layout

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,8 +1,14 @@
+import styles from './Sidebar.module.scss'
+import classNames from 'classnames/bind'
 import React from 'react'
+
+const cx = classNames.bind(styles)
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className={cx('wrapper')}>
+      <h2>Sidebar</h2>
+    </div>
   )
 }
 
--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,14 +1,18 @@
+import classNames from "classnames/bind";
 import Header from "~/components/Layout/components/Header";
+import styles from "./DefaultLayout.module.scss"
 import Sidebar from "./Sidebar";
 import React from 'react'
 
+const cx = classNames.bind(styles)
+
 const DefaultLayout = ({ children}) => {
   return (
-    <div>
+    <div className={cx('wrapper')}>
         <Header/>
-        <div className="container">
+        <div className={cx('container')}>
             <Sidebar/>
-            <div className="content">
+            <div className={cx('content')}>
                 {children}
             </div>
         </div>
